5669c77bad0d4057c0dfdff0224c6b384572042d
shkaff <mcshkaff@gmail.com>
2012-10-30 13:51:57 +0400

    Define constants

--- BLUE_estimation.py
+++ BLUE_estimation.py
@@ -5,3 +5,11 @@ Created on Tue Oct 30 13:46:53 2012
 @author: mikhail
 """
 
+#--------------------------------------------------------------------------------
+# Define constants
+#--------------------------------------------------------------------------------
+
+a = 10.0 # coupling constant
+hbar = 1.0 # plank constant
+m = 1.0 # mass of the oscillator
+w = 1.0 # eigenfrequency of the oscillator
